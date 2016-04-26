6240427e2901ae5d8a1c30d3cc361a2594cb38a6
darthburkhart <agburk@gmail.com>
2016-04-26 08:33:43 -0400

    new stuff

--- msb.v
+++ msb.v
@@ -17,6 +17,83 @@ module msb(in,out);
 	
 endmodule
 
+
+module addf(in1,in2,out);
+	input `WORD in1;
+	input `WORD in2;
+	output reg `WORD out;
+	
+	reg `WORD temp;
+	reg `WORD op1;
+	reg `WORD op2;
+	
+	reg[8:0] man1;
+	reg[8:0] man2;
+	reg[8:0] outMan;
+	
+	
+	always@(*) begin
+	
+		if (in2[14:7] > in1[14:7] ) begin
+			op2 = in1;
+			op1 = in2;
+		end else begin
+			op2 = in2;
+			op1 = in1;
+		end
+	
+		man1 = {2'b1,op1[6:0]};
+		man2 = {2'b1,op2[6:0]};
+		
+		man2 = man2 >> (op1[14:7] - op2[14:7]);
+		out[14:7] = (op1[14:7]);
+		if (op1[15] == op2[15]) begin
+			outMan = man1+man2;
+			$display("%b,%b,%b",man1,man2,outMan);
+			out[15] = op1[15];
+			if (outMan[8] == 1) //CHECK FOR OVERFLOW
+			begin
+				out[14:7] = out[14:7]-1; //MIGHT BREAK
+			end
+		end else begin
+		if (man1 > man2) begin
+			outMan = man1-man2;
+			out[15] = op1[15];
+		end else begin
+			outMan = man2-man1;
+			out[15] = op2[15];
+		end
+
+		end
+		out[6:0] = outMan[8:2];
+	
+	end	
+	
+endmodule
+	
+module i2f(in,out);
+	input `WORD in;
+	output reg `WORD out;
+	
+	reg[6:0] zeros;
+	reg [22:0] temp;
+	
+	wire `WORD numZeros;
+	
+	lead_zero leadZeros(in, numZeros);
+	
+	initial begin
+		zeros<= 0;
+	end
+	
+	always@(*) begin
+		temp = {in,zeros};
+		out[15] = in[15]; //set sign bit
+		out[14:7] = 127+(15-numZeros-1); //set exponent
+		out[6:0] = temp >> (15-numZeros); //set mantissa
+	end
+endmodule
+
 module lead_zero(in, out);
 
 	input `WORD in;
@@ -34,9 +111,10 @@ module lead_zero(in, out);
 			temp = temp | (temp>>8);
 			out = 16-vari;
 	end
+endmodule
 
 
-endmodule 
+ 
 module ones(in,out);
 	input `WORD in;
 	output reg `WORD out;
@@ -53,21 +131,44 @@ module ones(in,out);
 endmodule
 
 
+module f2i(in,out);
+	input `WORD in;
+	output reg `WORD out;
+	reg [22:0] temp;
+	
+	always @(*) begin
+		temp = {16'b1,in[6:0]};
+		//$display("%b",temp);
+		if (in[14:7] >= 127) begin
+			temp = temp << (in[14:7] - 127);
+			out = temp[22:7];
+		 end else begin 
+			out = 0;
+		end
+		if (in[15] == 1) begin
+			out = out ^ 'hffff;
+			out = out+1;
+		end
+	end	
+endmodule
+
 module test;
 
-	reg `WORD temp;
+	reg `WORD temp1;
+	reg `WORD temp2;
 	wire `WORD out;
 	
-	ones MSB(temp,out);
+	addf MSB(temp1,temp2,out);
 	
 	initial begin
-	temp <= 15;
+	temp1 <= 'h4040;
+	temp2 <= 'h4040;
 	
 	
-	repeat (10) begin
+	repeat (1) begin
 		#5;
-		$display("%d",out);
-		temp = temp * 2;
+		$display("%b",out);
+		//temp = temp * 2;
 	end
 	end
 	
